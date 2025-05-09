d1ca248ed18859555f6076ebd0ccf0f195a077cc
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2025-05-09 18:11:38 -0500

    Changing Avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 const Avatar = () => {
 	return (
-		<div className="pl-20 mb-10 hidden xl:flex xl:max-w-none">
+		<div className="pl-15 mb-10 hidden xl:flex xl:max-w-none">
 			<Image
 				src={"/1.png"}
-				width={1000}
-				height={1000}
+				width={800}
+				height={800}
 				alt=""
 				className="translate-z-0 w-full h-full"
 			/>
