bb5a8446682452d499fdbcb2d6ee0c31bd59a5e7
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2025-05-09 18:05:33 -0500

    Changing 6

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ const Home = () => {
 					animate="show"
 					exit="hidden"
 					transition={{ duration: 10, ease: "easeInOut" }}
-					className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-0">
+					className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[15%]">
 					<Avatar />
 				</motion.div>
 			</div>
