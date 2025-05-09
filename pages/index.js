c99facd24fb2fffe884c2167137f4f7ff8683c15
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2025-05-09 18:48:39 -0500

    Changing Avatar

--- pages/index.js
+++ pages/index.js
@@ -57,6 +57,8 @@ const Home = () => {
 			</div>
 			{/* image */}
 			<div className="w-[1200px] h-full absolute right-5 bottom-0">
+				{/* bg img */}
+				<div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
 				{/* particles */}
 				<ParticlesContainer />
 				{/* avatar img */}
