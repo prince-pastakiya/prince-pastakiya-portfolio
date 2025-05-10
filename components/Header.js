5ee05a05f5f808bd01e89cc8f7e6a3d1d69c053d
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2025-05-09 20:53:03 -0500

    Making it Perfect

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
 					{/* logo */}
 					<Link href={"/"}>
 						<Image
-							src={"/logo 1.svg"}
+							src={"/logo.svg"}
 							width={220}
 							height={48}
 							alt=""
