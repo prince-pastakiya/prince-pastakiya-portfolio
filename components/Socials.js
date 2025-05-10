7fd9b364fd6552610b4c7cfa395d575966a39e98
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2025-05-10 16:54:06 -0500

    Changign Layout

--- components/Socials.js
+++ components/Socials.js
@@ -13,7 +13,7 @@ import {
 
 const Socials = () => {
 	return (
-		<div className="flex items-center gap-x-5 text-lg">
+		<div className="flex items-center gap-x-5 text-lg mb-10">
 			<Link
 				href={"https://www.linkedin.com/in/prince-pastakiya/"}
 				className="hover:text-accent transition-all duration-300 ">
