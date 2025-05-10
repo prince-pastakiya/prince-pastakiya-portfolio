e6028a349e320620a240d3450bcaf91fea2280dd
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2025-05-09 20:29:47 -0500

    changing avatar

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -18,7 +18,7 @@ const ProjectsBtn = () => {
 					width={250}
 					height={250}
 					alt=""
-					className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+					className="animate-spin-slow w-full h-full max-w-[200px] max-h-[200px]"
 				/>
 				<HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
 			</Link>
