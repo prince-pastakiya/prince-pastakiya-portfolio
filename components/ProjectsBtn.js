1f7268e6b92e6f438ad041d4b3859d0b3bbf5b02
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2025-05-09 20:39:39 -0500

    Changing Href

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -11,7 +11,7 @@ const ProjectsBtn = () => {
 	return (
 		<div className="mx-auto xl:mx-0 z-10">
 			<a
-				src="/files/Resume.pdf"
+				href={"/files/Resume.pdf"}
 				className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group">
 				<Image
 					src={"/rounded-text.png"}
