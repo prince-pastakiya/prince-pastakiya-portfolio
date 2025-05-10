b2249ab3297baab59322931be5210d4bcd5b5a0c
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2025-05-10 16:45:14 -0500

    Changing Layout

--- pages/index.js
+++ pages/index.js
@@ -19,15 +19,15 @@ const Home = () => {
 			<div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
 				<div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
 					{/* title */}
-					<motion.h1
+					<motion.h2
 						variants={fadeIn("down", 1)}
 						initial="hidden"
 						animate="show"
 						exit="hidden"
-						className="h1">
+						className="h2">
 						Leveraging Data to <br />
 						<span className="text-accent">Catalyze Impact.</span>
-					</motion.h1>
+					</motion.h2>
 					{/* subtitle */}
 					<motion.p
 						variants={fadeIn("right", 2)}
