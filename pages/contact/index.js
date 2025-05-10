28a9bfaf4c5bee3dd4bb88e7b18bbc421f448a5d
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2025-05-09 19:12:32 -0500

    Changing Avatar

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -47,8 +47,8 @@ const Contact = () => {
 						</button>
 					</motion.form>
 				</div>
+				<div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0 pl-20"></div>
 			</div>
-			<div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0 pl-20"></div>
 		</div>
 	);
 };
